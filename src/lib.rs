38f252e7272d84a51fb93eb5b23bdf778ee8cdd0
Jonathan Wesley Stone <js@piratejon.com>
2015-03-10 22:52:04 -0500

    implement default value for Bst

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
 
+pub struct Bst<T> {
+  left : std::option::Option<std::boxed::Box<Bst<T>>>,
+  right : std::option::Option<std::boxed::Box<Bst<T>>>,
+  payload : std::option::Option<std::boxed::Box<T>>
+}
+
+impl<T> std::default::Default for Bst<T> {
+  fn default() -> Bst<T> {
+    Bst::<T> {
+      left : None,
+      right : None,
+      payload: None
+    }
+  }
+}
+
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,5 +1,12 @@
+extern crate rustree;
+
 #[test]
 fn it_works() {
   assert!(true);
+}
+
+#[test]
+fn init_bst() {
+  let bst: rustree::Bst<u8> = std::default::Default::default();
 }
 
